25516afe70f6e08b91e189dd27f1bf643c2b5961
Rômulo <romulopinheirocosta@gmail.com>
2021-04-29 03:51:47 -0300

    Update control.c
    Add getSemitone function
    Fix scaleOf function

--- control.c
+++ control.c
@@ -2,12 +2,22 @@
 #include <math.h>
 
 
+double getSemitone(double frequency, int grade);
 double nextSemitone(double frequency);
 double previousSemitone(double frequency);
 int pitch(double frequency);
 int scaleOf(double frequency);
 
-// return the next semiton of a frequency in Hz
+
+// return desired semitone of a frequency in Hz
+double getSemitone(double frequency, int grade)
+{
+	double semitone = frequency * pow(2, (double) grade/12);
+
+	return semitone;
+}
+
+// return the next semitone of a frequency in Hz
 double nextSemitone(double frequency)
 {
 	/*	A4  = 440Hz; A5  = 880Hz
@@ -20,7 +30,7 @@ double nextSemitone(double frequency)
 	return frequency * semitone;
 }
 
-// return the previous semiton of a frequency in Hz
+// return the previous semitone of a frequency in Hz
 double previousSemitone(double frequency)
 {
 	double semitone = pow(2, 0.083333);
@@ -43,34 +53,25 @@ int pitch(double frequency)
 	return 0;
 }
 
-// Print a music scale starting from A's frequency. A = 440.00
+// Print a music scale of a input frequency
 int scaleOf(double frequency) 
 {
 
 	double semitone = pow(2, 0.083333);			// a semiton distance
 	int note;												// Musical notes of a scale
+	char* grade[12] = {"1ª", "2ªm", "2ª", "3ªm", "3ª", "4ª", "4ª+", "5ª", "5ª+", "6ª", "7ªm", "7ª"};
 
 
 	// print the music intervals and its frequency value
 	printf("Interval\tFrequency\tNote\n");
-	for (note=1;note<8;note++)
+	for (note=0;note<12;note++)
 	{
-		if (note == 3 | note == 7)
-		{
-			printf("%dª\t-\t%.2f\t-\t", note, frequency);
-			pitch(frequency);
-			frequency *= semitone;
+		printf("%s\t\t%.2lf\t\t", \
+						grade[note], getSemitone(frequency, note));
+		pitch(getSemitone(frequency, note));
 
-		}else 
-		{
-			printf("%dª\t-\t%.2f\t-\t", note, frequency);
-			pitch(frequency);
-			frequency *= semitone;
-			printf("%dªm\t-\t%.2f\t-\t", note+1, frequency);
-			pitch(frequency);
-		}
-		frequency *= semitone;
 	}
-
 	return 0;
 }
+
+
